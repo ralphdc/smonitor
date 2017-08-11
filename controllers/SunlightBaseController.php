17f98874d2e6898ac64f06d8ba406a0e9ddc9401
chao.dong <472298551@qq.com>
2017-08-12 00:44:21 +0800

    add new info

---
+++ controllers/SunlightBaseController.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\controllers;
+
+/**
+* 
+*/
+class SunlightBaseController extends Controller
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+
+	function actionTest()
+	{
+		echo "Hello World~!";
+		exit;
+	}
+}
